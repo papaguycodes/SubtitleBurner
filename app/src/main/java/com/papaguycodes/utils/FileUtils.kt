e3d36f2a0745113c27f66b05c4eb8a50d51ace57
PaPaGuy Codes  / <175421249+PaPaGuyCodes@users.noreply.github.com>
2024-08-11 21:13:43 +0100

    Create FileUtils.kt

---
+++ app/src/main/java/com/papaguycodes/utils/FileUtils.kt
@@ -0,0 +1,17 @@
+package com.papaguycodes.subtitleburner.utils
+
+import android.content.Context
+import android.net.Uri
+
+object FileUtils {
+
+    fun getFileName(context: Context, uri: Uri): String {
+        // Code to extract file name from Uri
+        return ""
+    }
+
+    fun getOutputUri(context: Context, fileName: String, directoryUri: Uri): Uri {
+        // Code to generate an output Uri based on fileName and directoryUri
+        return Uri.EMPTY
+    }
+}
